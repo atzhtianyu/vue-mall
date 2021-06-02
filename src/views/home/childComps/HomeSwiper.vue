1d5928fde1046acffd1e97575a4f9b3a4cefb4b3
zhtianyu <1420098499@qq.com>
2021-06-02 19:56:25 +0800

    swiper

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <swiper>
+      <swiper-item v-for="(item, id) in banners" :key="id">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import {Swiper, SwiperItem} from "@/components/common/swiper"
+
+export default {
+  name: "HomeSwiper",
+  props: {
+    banners: {
+      Array,
+      default() {
+        return []
+      }
+    }
+  },
+  components: {
+    Swiper,
+    SwiperItem
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
