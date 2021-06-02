d95cce011fc899cfa12d65991b4f2c746442bbcc
zhtianyu <1420098499@qq.com>
2021-06-02 19:57:26 +0800

    home

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,25 +5,134 @@
         <div>购物街</div>
       </template>
     </nav-bar>
+    <scroll class="content">
+      <home-swiper :banners="banners"></home-swiper>
+      <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control class="tab-control"
+                   :titles="['流行','新款','精选']"
+                   @tabClick="tabClick"></tab-control>
+      <goods-list :goods="goods[currentType].list"></goods-list>
+    </scroll>
   </div>
 </template>
 
 <script>
+import HomeSwiper from "@/views/home/childComps/HomeSwiper";
+import RecommendView from "@/views/home/childComps/RecommendView";
+import FeatureView from "@/views/home/childComps/FeatureView";
+import GoodsList from "@/components/content/goods/GoodsList";
+
 import NavBar from "@/components/common/navbar/NavBar";
+import TabControl from "@/components/content/tabControl/TabControl";
+import Scroll from "@/components/common/scroll/Scroll";
+
+import {getHomeMultidata, getHomeGoods} from "@/network/home";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': {page: 0, list: []},
+      },
+      currentType: 'pop'
+    }
+  },
   components: {
-    NavBar
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    GoodsList,
+    NavBar,
+    TabControl,
+    Scroll
+  },
+  created() {
+    this.getHomeMultidata();
+
+    this.getHomeGoods('pop');
+    this.getHomeGoods('new');
+    this.getHomeGoods('sell');
+  },
+  methods: {
+    /**
+     * 事件监听
+     */
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = 'pop';
+          break;
+        case 1:
+          this.currentType = 'new';
+          break;
+        case 2:
+          this.currentType = 'sell';
+          break;
+      }
+    },
+    /**
+     * 网络请求
+     */
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      })
+    }
   }
 }
 </script>
 
 <style scoped>
 
+#home {
+  padding-top: 44px;
+  height: 100vh;
+}
+
 .nav-bar {
-  background-color: #ff8198;
+  position: fixed;
+  right: 0;
+  left: 0;
+  top: 0;
+  z-index: 1;
+  background-color: var(--color-tint);
   color: #fff;
 }
+
+.tab-control {
+  position: sticky;
+  top: 44px;
+  z-index: 9;
+}
+
+.content {
+  overflow: hidden;
+
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
+
+/*.content {*/
+/*  height: calc(100% - 93px);*/
+/*  overflow: hidden;*/
+/*  margin-top: 44px;*/
+/*}*/
 
 </style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="recommend">
+    <div v-for="(item, id) in recommends" :key="id" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props: {
+    recommends: {
+      Array,
+      default() {
+        return [];
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.recommend {
+  display: flex;
+  width: 100%;
+  text-align: center;
+  padding: 10px 0 20px;
+  font-size: 12px;
+  border-bottom: 10px solid #eee;
+}
+
+.recommend-item {
+  flex: 1;
+
+}
+
+.recommend-item img {
+  width: 70px;
+  height: 70px;
+  margin-bottom: 10px;
+}
+
+</style>
