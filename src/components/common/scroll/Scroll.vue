69fc7833b47489a5b0c7a52e7c56f2ae062110ad
zhtianyu <1420098499@qq.com>
2021-06-02 20:03:21 +0800

    scroll

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll';
+import ObserveDOM from '@better-scroll/observe-dom'
+BScroll.use(ObserveDOM)
+
+export default {
+  name: "Scroll",
+  data() {
+    return {
+      scroll: null
+    }
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      probeType: 3,
+      click: true,
+      observeDOM: true
+      // updated: true
+    })
+    this.scroll.on('scroll', (position) => {
+      console.log(position);
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
