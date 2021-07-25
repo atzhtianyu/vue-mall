6d341f2ac57bfa3b28ca0c60fa1e8970ee6c5abd
zhtianyu <1420098499@qq.com>
2021-07-25 22:00:56 +0800

    profile

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -3,14 +3,39 @@
     <nav-bar class="profile-nav-bar">
       <div slot="center">小码哥商城</div>
     </nav-bar>
+    <!--1.单独封装一个组件-->
     <user-info></user-info>
 
+    <!--2.没有单独封装: 不同的地方太多, 需要传过多的参数-->
+    <section class="account">
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0.00</span> 元
+        </div>
+        <div class="account-info">我的余额</div>
+      </div>
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0</span> 个
+        </div>
+        <div class="account-info">我的优惠</div>
+      </div>
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0</span> 分
+        </div>
+        <div class="account-info">我的积分</div>
+      </div>
+    </section>
+    <list-view :list-data="orderList" class="order-list"></list-view>
+    <list-view :list-data="serviceList" class="service-list"></list-view>
   </div>
 
 </template>
 
 <script>
 import UserInfo from "@/views/profile/childComps/UserInfo";
+import ListView from "@/views/profile/childComps/ListView";
 
 import NavBar from "@/components/common/navbar/NavBar";
 
@@ -18,17 +43,69 @@ export default {
   name: "Profile",
   components: {
     NavBar,
-    UserInfo
+    UserInfo,
+    ListView
   },
-
+  data() {
+    return {
+      orderList: [
+        {icon: '#order', iconColor: '#ff8198', info: '我的消息'},
+        {icon: '#point', iconColor: '#fc7b53', info: '积分商城'},
+        {icon: '#vip', iconColor: '#ffc636', info: '会员卡'},
+      ],
+      serviceList: [
+        {icon: '#service', iconColor: '#ff8198', info: '我的购物车'},
+        {icon: '#download', iconColor: '#ff8198', info: '下载购物APP'},
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
 
+.profile {
+  background-color: #f2f2f2;
+}
+
 .profile-nav-bar {
   color: #fff;
   background-color: var(--color-tint);
+}
+
+.account {
+  display: flex;
+}
+
+.account-item {
+  width: 100%;
+  background-color: #fff;
+  margin-right: 1px;
+  text-align: center;
+}
+
+.account-item:last-of-type {
+  margin-right: 0;
+}
+
+.account-item {
+  color: #666;
+  font-size: 13px;
+  padding: 18px;
+}
+
+.account-item .balance {
+  font-size: 24px;
+  font-weight: 700;
+  color: orangered;
+}
+
+.account-info {
+  margin-top: 6px;
+}
+
+.order-list, .service-list {
+  margin-top: 12px;
 }
 
 </style>
---
+++ src/views/profile/childComps/ListView.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="view-list">
+    <div v-for="(item, index) in listData" :key="index" class="item">
+      <span class="icon">
+        <svg :fill="item.iconColor"><use :xlink:href="item.icon"></use></svg>
+      </span>
+      <div class="info">{{item.info}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListView",
+  props: {
+    listData: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.view-list {
+  background-color: #fff;
+  font-size: 15px;
+  color: #333;
+}
+
+.view-list .item {
+  height: 44px;
+  line-height: 44px;
+  position: relative;
+}
+
+.icon svg {
+  width: 18px;
+  height: 18px;
+}
+
+.item .icon {
+  margin-top: 10px;
+  position: absolute;
+  left: 16px;
+  top: -7px;
+}
+
+.item .info {
+  margin-left: 40px;
+  border-bottom: 1px solid rgba(100, 100, 100, .1);
+  padding-left: 5px;
+}
+
+.item:last-of-type .info {
+  border-bottom: none;
+}
+
+</style>
